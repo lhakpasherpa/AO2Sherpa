69b4bbcf61d87bd4e8c73834e67d6cdf0e4bd93d
Lhakpa Sherpa <s525286@nwmissouri.edu>
2017-10-01 19:54:19 -0500

    asd

--- AO2Sherpa/Calculator.js
+++ AO2Sherpa/Calculator.js
@@ -1,12 +1,32 @@
 function calculator() {
   var total = 0;
-  var itemPrice = document.getElementById('a1').value;
-  var itemNumber = document.getElementById('a2').value;
-  var itemDiscount = document.getElementById('a3').value;
-  while(itemPrice !== 0){
-     total = itemPrice*itemNumber*(itemDiscount/100);
-    console.log(total);
+  var itemPrice = (document.getElementById("a1").value);
+  var itemNumber = (document.getElementById("a2").value);
+  var itemDiscount =(document.getElementById("a3").value);
+  if (isNaN(itemPrice)) 
+  {
+    alert("Your input must be a numbers");
+    return false;
   }
-  return total;
+  if (isNaN(itemNumber)) 
+  {
+    alert("Your input must be a numbers");
+    return false;
+  }
+  if (isNaN(itemDiscount)) 
+  {
+    alert("Your input must be a numbers");
+    return false;
+  }
+    else if(itemNumber == 0){
+    alert("You can't have item number zero");
+     }
+    else if(itemPrice == 0){
+     window.alert("You cant have item price zero");
+     }
+     else{
+     var  total = (itemPrice*itemNumber-(itemPrice*itemDiscount/100));
+    document.getElementById("Calculate").innerHTML="The price of the item is: "+ total;
+  
+  }
 }
-document.getElementById("calculate").innerHTML=calculator();
